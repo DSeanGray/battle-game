84ce36d37aa0c4331074db8af69fb048c1dde855
david gray <davidseangray@gmail.com>
2017-04-26 15:56:49 +0100

    can view player hitpoints

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -1,6 +1,6 @@
 
 
-describe 'Name form fill', :type => :feature do 
+describe 'Name form fill', :type => :feature do
 	it 'Displays names submitted through form' do
 		visit '/'
 		within('form') do
@@ -8,7 +8,16 @@ describe 'Name form fill', :type => :feature do
 			fill_in 'player_2', with: "David"
 		end
 		click_button 'Submit'
-		expect(page).to have_content 'Sam, David'
+		expect(page).to have_content 'Sam vs David'
 	end
-end
 
+	feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
+    visit('/')
+    fill_in :player_1, with: 'Sam'
+    fill_in :player_2, with: 'David'
+    click_button 'Submit'
+    expect(page).to have_content 'Sam: 60HP'
+  end
+end
+end
