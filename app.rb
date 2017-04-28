2851f60a66fa6b5cdda939fb6651b9090ad24fab
david gray <davidseangray@gmail.com>
2017-04-28 11:40:54 +0100

    game class now refactored to reduce controllers to skinny controllers

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require_relative 'models/player'
+require_relative 'models/game'
 
 class Battle < Sinatra::Base
 enable :sessions
@@ -8,22 +9,25 @@ enable :sessions
     erb :index
   end
 
-  get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    erb :play
-  end
-
   post '/name' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    $game = Game.new(player_1, player_2)
     redirect('/play')
   end
 
+  get '/play' do
+    @game = $game
+    # @player_1 = $game.player_1.name
+    # @player_2 = $player_2.name
+    erb :play
+  end
+
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    # @player_1 = $player_1
+    # @player_2 = $player_2
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../models/game'
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player_1) { double :player }
-  let(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2}
 
   describe '#attack' do
     it 'damages the player' do
@@ -10,4 +10,14 @@ describe Game do
       game.attack(player_2)
     end
   end
+
+    it 'gets an instance of player 1' do
+      expect(game.player_1).to eq player_1
+    end
+
+    it 'gets an instance of player 2' do
+      expect(game.player_2).to eq player_2
+    end
+
+
 end
