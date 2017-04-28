3179d109c7b7dba4531cff282ae4367a4ad212a6
david gray <davidseangray@gmail.com>
2017-04-28 13:06:12 +0100

    players can see whose turn it is after attack

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -4,4 +4,10 @@ feature 'Switch turns' do
     sign_in_and_play
     expect(page).to have_content "Sam's Turn"
   end
+
+  scenario 'Player can see whose turn it is after attack' do
+    sign_in_and_play
+    click_button('attack')
+    expect(page).to have_content "David's Turn"
+  end
 end
